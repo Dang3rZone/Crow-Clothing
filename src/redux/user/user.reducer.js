285918acf35a01f60b01e9fe98c5dabb47c46188
Andres <andresrosalespinzon@gmail.com>
2021-04-13 16:16:06 +0200

    redirect to home if user is signed in and created user type for best practices for setting  current user

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTpes } from './user.types';
+
 const INITIAL_STATE = {
   currentUser: null,
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case UserActionTpes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
