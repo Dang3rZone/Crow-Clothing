a4cd5c8d02fdfcb8d98648f68c72f698552af270
Andres <andresrosalespinzon@gmail.com>
2021-04-21 19:22:13 +0200

    changed code to use redux thunk and use of containers

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,33 @@
+import {
+  convertCollectionsSnapshotToMap,
+  firestore,
+} from '../../firebase/firebase.utils';
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = (collectionMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collectionMap,
 });
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+    collectionRef
+      .get()
+      .then((snapShot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapShot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
