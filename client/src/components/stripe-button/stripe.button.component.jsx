c82398fc29f939a00c5644526352b7275b93f285
Andres <andresrosalespinzon@gmail.com>
2021-04-24 13:58:33 +0200

    express server and front into client server

--- client/src/components/stripe-button/stripe.button.component.jsx
+++ client/src/components/stripe-button/stripe.button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeCheckoutButton = ({ price }) => {
     'pk_test_51IhXPKBuuazJwXehxeEwbDOMNgvu8NjEcqSQoFRYWz3QxjvLSkLwDywItWMDPQliJVxdXJ9TZTZ0d274MlcbHunK00TlSJjdqO';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful!');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment succesful');
+      })
+      .catch((error) => {
+        console.log('Payment eror ', JSON.parse(error));
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
 
   return (
